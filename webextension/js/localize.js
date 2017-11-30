4a615b87d38d9894537c8bba792033619c342d2a
Nagy István <34108798+Csiliz@users.noreply.github.com>
2017-11-30 01:19:01 +0100

    Javascript file for localization html files

---
+++ webextension/js/localize.js
@@ -0,0 +1,22 @@
+﻿(function() {
+	//Localize by replacing __MSG_***__ meta tags
+	try {
+		//var objects = document.getElementsByTagName('html');
+		var objects = document.getElementsByTagName('localizable');
+		for (var j = 0; j < objects.length; j++) {
+			var obj = objects[j];
+			var valStrH = obj.innerHTML.toString();
+			var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1) {
+				return v1 ? browser.i18n.getMessage(v1) : "";
+			});
+
+			if(valNewH != valStrH) {
+				obj.innerHTML = valNewH;
+				console.log(valNewH);
+			}
+	    }
+	}
+	catch (e) {
+		console.log("Localization error: " + e.message);
+	}
+})();
